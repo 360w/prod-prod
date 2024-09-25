ef8aeed6a630448ff4923f293eefece35259b6e7
360w <dkorolev311@gmail.com>
2024-09-25 16:32:28 +0300

    lang switcher button added+translate added (i18next)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from './providers/ThemeProvider';
 import { AppRouter } from './providers/router';
@@ -7,16 +7,19 @@ import { SideBar } from 'widgets/SideBar';
 
 
 
+
 const App: FC = () => {
     const { theme } = useTheme();
 
     return (
         <div className={classNames('app', { hovered: true, selected: false, red: false }, [theme, 'cls1', 'cls2'])}>
-            <NavBar />
-            <div className='content-page'>
-                <SideBar />
-                <AppRouter />
-            </div>
+            <Suspense fallback>
+                <NavBar />
+                <div className='content-page'>
+                    <SideBar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -1,17 +1,21 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './NavBar.module.scss';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 
 interface NavBarProps {
     className?: string;
 }
 
 export const NavBar = ({ className }: NavBarProps) => {
+
+    const { t } = useTranslation('navbar');
+
     return (
         <div className={classNames(cls.NavBar, {}, [className])}>
             <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.PRIMARY} className={cls.mainLink} to={'/'}>Главная</AppLink>
-                <AppLink theme={AppLinkTheme.PRIMARY} className={cls.aboutLink} to={'/about'}>О сайте</AppLink>
+                <AppLink theme={AppLinkTheme.PRIMARY} className={cls.mainLink} to={'/'}>{t('Главная')}</AppLink>
+                <AppLink theme={AppLinkTheme.PRIMARY} className={cls.aboutLink} to={'/about'}>{t('О сайте')}</AppLink>
             </div>
         </div>
     );
